aa914d6fbb895691077c76ae1016f0552c9caadd
Brian Haas <brianjohnhaas@gmail.com>
2019-04-19 15:27:37 -0400

    add

---
+++ uger/centrifuge_fasta_screener_cmd_generator.pl
@@ -0,0 +1,20 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+
+while(<>) {
+    my $file = $_;
+    if (-f $file) {
+        my $cmd = "singularity run "
+            . " -e -B /seq/RNASEQ /seq/RNASEQ/TOOLS/SINGULARITY/ctat-centrifuge/ctat_centrifuge.v0.0.1.simg "
+            . " centrifuge -p 1 -k 1 -f "
+            . " -U $file "
+            . " -x /seq/RNASEQ/__ctat_genome_lib_building/centrifuge_indexes/abvh  "
+            . " --report-file $file.centrifuge ";
+
+        print "$cmd\n";
+    }
+}
+
